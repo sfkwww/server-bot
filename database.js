0c46b0a2d65498fccc7b930b1cdb9ad545ba990d
sfkwww <sebbe.williams@gmail.com>
2020-04-05 02:43:06 +0200

    Change db location

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-const databasePath = path.join(__dirname, 'db.sqlite');
+const databasePath = path.join(__dirname, './\.data/db.sqlite');
 const db = new sqlite3.Database(databasePath);
 
 // INIT
